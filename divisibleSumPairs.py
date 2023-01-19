67471b11f4af0169d8284543fe15c105efc6a8e3
Yeonjee Choi <47962204+chbigbrother@users.noreply.github.com>
2023-01-19 17:47:28 +0900

    Create divisibleSumPairs.py

---
+++ divisibleSumPairs.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    # Write your code here
+    cnt = 0
+
+    for i in range(len(ar)):
+        for j in range(len(ar[i+1:])):
+            n = (ar[i] + ar[j+i+1]) / k
+            it = int(n)
+            if n == it:   
+                cnt += 1
+            
+    return cnt;
